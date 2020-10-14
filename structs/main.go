48e8fbcb7712483d2557b09275faac419e24e89b
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2020-10-14 14:49:20 +0300

    Pointers

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,43 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	//alex := person{"Alex", "Anderson"}
 
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// var nick person
+	// fmt.Println(nick)
+	// fmt.Printf("%+v", alex)
 
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Whatever",
+		contactInfo: contactInfo{
+			email:   "whatever@whatever.com",
+			zipCode: 2133,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
