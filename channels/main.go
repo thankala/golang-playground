9b3e0c585bf42c6235cfeddb01a4b657440559bf
Thanasis Kalatheris <kalatheristhanasis@gmail.com>
2020-10-15 22:22:56 +0300

    Finished channels and routines

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, url := range links {
+		go checkLink(url, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	for l := range c {
+		go func(l string) {
+			time.Sleep(2 * time.Second)
+			checkLink(l, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+
+	_, err := http.Get(link)
+	if err != nil {
+		log.Println(err)
+		c <- link
+		return
+	}
+
+	fmt.Println(link, " is up")
+	c <- link
 }
